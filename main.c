97b00a4b01c89bc8e960cbe64b7a7c79f1667e45
SiqueiraBastos <siqueira.bastos@hotmail.com>
2022-01-25 09:36:33 -0300

    primeiro exercicio

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a, b, x;
+
+    printf("Entre com o valor de a: ");
+    scanf("%i", &a);
+
+    printf("Entre com o valor de b: ");
+    scanf("%i", &b);
+
+    printf("O valor da soma eh: %i", x = a + b);
+
+   // return 0;
+
+}
